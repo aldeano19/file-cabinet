15004625f7a580999cfed1dc17c490bbee645031
dibu2018 <erieljr1@gmail.com>
2018-06-11 02:21:11 -0400

    eddit and destroy docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -27,9 +27,16 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.updated_at(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
